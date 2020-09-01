1fb4819a523de809f8db12d7ca30fb44aa2bdeb9
Wangoru Kihara <soloincc@gmail.com>
2020-09-01 08:04:14 +0300

    Historical changes

--- static/js/badili_dash.js
+++ static/js/badili_dash.js
@@ -1669,3 +1669,12 @@ BadiliDash.prototype.refreshViewData = function(){
 };
 
 var dash = new BadiliDash();
+
+function objectifyForm(formArray) {
+    //serialize data function
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+        returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+}
