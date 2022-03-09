e97e4d22225e0b639296509b5a02ec23f0aeae0a
Wangoru Kihara <soloincc@gmail.com>
2022-03-10 02:12:42 +0300

    Drop six package requirement as it is not being used

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ from django.shortcuts import render, redirect
 from django.middleware import csrf
 from django_registration.exceptions import ActivationError
 from django_registration.backends.activation.views import RegistrationView, ActivationView
-from django.utils import six, timezone
+from django.utils import timezone
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.urls import reverse
